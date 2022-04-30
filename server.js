38f52381f0a402b8dad6690efa8e72bad667581c
Antonio Sabino <aslimajr@outlook.com>
2022-04-30 18:53:19 -0300

    Instalação do Socket.io e estabelecendo conexão

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const path = require("path");
 const http = require("http");
+const socketIO = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+const io = socketIO(server);
 
 server.listen(3000, () => {
   console.log("Estamos online");
 });
 
 app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  console.log("Conectado IO");
+});
